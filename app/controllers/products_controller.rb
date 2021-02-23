02bdc18f324f54d6f4d3981c8b085c0112f262ba
aperezsaez <alex.perez.22@live.com>
2021-02-23 11:35:24 -0800

    Finished Stage 4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -72,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :stock, :image, :q, :user_id)
+      params.require(:product).permit(:name, :price, :stock, :image, :q, :user_id, :photo)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     paginates_per 20
     belongs_to :user
+    has_one_attached :photo
 end
